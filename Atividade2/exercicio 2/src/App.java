afc66b486af33d4da59471abf011bab30ffa08ce
alexxdoug <115509422+alexxdoug@users.noreply.github.com>
2022-10-10 23:07:20 -0300

    Add files via upload

---
+++ Atividade2/exercicio 2/src/App.java
@@ -0,0 +1,12 @@
+import java.util.Scanner;
+
+public class app {
+    public static void main(string[] args) throws Exception {
+        float numero = 0;
+
+        Scanner S = new Scanner (system.in);
+        System.out.println("digite o numero");
+        numero = S.nextFloat();
+        System.out.println("o numero digitado foi" + numero);
+    }
+}
